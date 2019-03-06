28feee31db6d5d597e7fc6a5eb817dfc4483ca2d
samho <samhopereino@gmail.com>
2019-03-05 20:51:31 -0500

    Got Sign Up working. Very unrefined and ugly but it works. Yay.

--- app/src/main/java/com/example/signfinder/LoginActivity.java
+++ app/src/main/java/com/example/signfinder/LoginActivity.java
@@ -1,37 +1,49 @@
 package com.example.signfinder;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText username;
     EditText password;
-    String uName, pWord = "";
+    TextView message;
+    String uName, pWord, logInMessage = "";
+    String resultS;
+    int signIn = 0;
 
     public void signUp() {
-        mainActivity();
-        //check database for username is unique
-        //if (username is unique && password is not blank) {
-            //allow sign up (put info in database)
-            //start mainActivity
-        //else if(password is blank)
-            //display message about password not being blank
-        //else {
-            //display message about username not being unique
+        uName = username.getText().toString();
+        pWord = password.getText().toString();
+        signIn = 0;
+
+        new LoginAsync().execute(uName, pWord);
     }
 
     public void logIn() {
-        mainActivity();
-        //check username/password match
-        //if(both match)
-            //allow logIn and start mainActivity
-        //else
-            //display message about username/password are incorrect
+        uName = username.getText().toString();
+        pWord = password.getText().toString();
+        signIn = 1;
     }
 
     public void mainActivity() {
@@ -48,11 +60,13 @@ public class LoginActivity extends AppCompatActivity {
         username = (EditText)findViewById(R.id.userNameBox);
         password = (EditText)findViewById(R.id.passwordBox);
 
+        message = (TextView)findViewById(R.id.logInMessage);
+
         Button signUp = (Button)findViewById(R.id.signUpButton);
         Button logIn = (Button)findViewById(R.id.logInButton);
 
-        uName = username.getText().toString();
-        pWord = password.getText().toString();
+        message = (TextView)findViewById(R.id.logInMessage);
+        message.setVisibility(View.INVISIBLE);
 
         signUp.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -65,5 +79,130 @@ public class LoginActivity extends AppCompatActivity {
                 logIn();
             }
         });
+    }
+
+    public class LoginAsync extends AsyncTask<String, String, String> {
+
+        public String dbUName, dbPWord = "";
+
+        public LoginAsync() {
+
+        }
+
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected String doInBackground(String... arg0) {
+            if(signIn == 0) {
+                try {
+                    dbUName = (String)arg0[0];
+                    dbPWord = (String)arg0[1];
+                    String link = "http://ec2-18-219-194-235.us-east-2.compute.amazonaws.com/db_signInCheck.php?username="+dbUName+"&password="+dbPWord;
+
+                    URL url = new URL(link);
+                    HttpClient client = new DefaultHttpClient();
+                    HttpGet request = new HttpGet();
+                    request.setURI(new URI(link));
+                    HttpResponse response = client.execute(request);
+                    BufferedReader in = new BufferedReader(new
+                            InputStreamReader(response.getEntity().getContent()));
+
+                    StringBuffer sb = new StringBuffer("");
+                    String line = null;
+
+                    // Read Server Response
+                    while((line = in.readLine()) != null) {
+                        sb.append(line);
+                        break;
+                    }
+
+                    resultS = sb.toString();
+
+
+
+
+
+
+
+                    boolean help = resultS.equals(" 0 ");
+                    if(help == true) {
+                        try {
+                            link = "http://ec2-18-219-194-235.us-east-2.compute.amazonaws.com/db_signIn.php?username="+dbUName+"&password="+dbPWord;
+
+                            url = new URL(link);
+                            client = new DefaultHttpClient();
+                            request = new HttpGet();
+                            request.setURI(new URI(link));
+                            response = client.execute(request);
+                            in = new BufferedReader(new
+                                    InputStreamReader(response.getEntity().getContent()));
+
+
+                        }
+                        catch(Exception e) {
+                            logInMessage = e.getMessage();
+                            message.setText(logInMessage);
+                            message.setVisibility(View.VISIBLE);
+                        }
+
+                    }
+                    else {
+                        //username already in user
+                    }
+
+
+
+
+
+                    return resultS;
+                } catch(Exception e) {
+                    logInMessage = "Error: " + e.getMessage();
+                    message.setText(logInMessage);
+                    message.setVisibility(View.VISIBLE);
+                }
+            }
+            else {
+                try {
+                    dbUName = (String)arg0[0];
+                    dbPWord = (String)arg0[1];
+                    String link = "http://ec2-18-219-194-235.us-east-2.compute.amazonaws.com/db_signInCheck.php?username="+dbUName+"&password="+dbPWord;
+
+                    URL url = new URL(link);
+                    HttpClient client = new DefaultHttpClient();
+                    HttpGet request = new HttpGet();
+                    request.setURI(new URI(link));
+                    HttpResponse response = client.execute(request);
+                    BufferedReader in = new BufferedReader(new
+                            InputStreamReader(response.getEntity().getContent()));
+
+                    StringBuffer sb = new StringBuffer("");
+                    String line = null;
+
+                    // Read Server Response
+                    while((line = in.readLine()) != null) {
+                        sb.append(line);
+                        break;
+                    }
+
+                    resultS = sb.toString();
+
+
+                    return resultS;
+                } catch(Exception e) {
+                    logInMessage = "Error: Can't check database";
+                    message.setText(logInMessage);
+                    message.setVisibility(View.VISIBLE);
+                }
+            }
+
+            return resultS;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+        }
     }
 }
